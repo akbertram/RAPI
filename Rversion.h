ecdc22ccd17862aaeec1cd14bcfc8b65c7601160
Lukas Stadler <lukas.stadler@oracle.com>
2016-08-05 11:20:40 +0200

    adding header files

---
+++ Rversion.h
@@ -0,0 +1,25 @@
+/* Rversion.h.  Generated automatically. */
+#ifndef R_VERSION_H
+#define R_VERSION_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define R_VERSION 1234
+#define R_NICK "Nickname"
+#define R_Version(v,p,s) (((v) * 65536) + ((p) * 256) + (s))
+#define R_MAJOR  "3"
+#define R_MINOR  "4.5"
+#define R_STATUS "Under development (unstable)"
+#define R_YEAR   ""
+#define R_MONTH  ""
+#define R_DAY    ""
+#define R_SVN_REVISION 123456
+#define R_FILEVERSION    3,45,,6
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* not R_VERSION_H */
